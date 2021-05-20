b4d04655d0f61cecc61fad4cd5cd818220e990e2
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-05-20 14:28:37 +0000

    a simpler implementation of clic_subset

--- R/clic_subset.R
+++ R/clic_subset.R
@@ -1,36 +1,22 @@
-subset_df <- function(x, metadata = FALSE) {
+subset_df <- function(response_data, metadata = FALSE) {
   
-  left <- paste(unlist(head(x[[1]], -1)), collapse = "")
-  left_words <- unlist(tail(x[[1]], 1))
-  
-  node <- paste(unlist(head(x[[2]], -1)), collapse = "")
-  node_words <- unlist(tail(x[[2]], 1))
-  
-  right <- paste(unlist(head(x[[3]], -1)), collapse = "")
-  right_word <- unlist(tail(x[[3]], 1))
-  
-  result_metadata <- x[[4]]
-  position_metadata <- x[[5]]
-  
-  book_title <- result_metadata[[1]]
-  
-  X <- data.frame(
-    left             = left,
-    node             = node,
-    right            = right,
-    book             = book_title,
+  result <- data.frame(
+    left             = paste(unlist(head(response_data[[1]], -1)), collapse = ""),
+    node             = paste(unlist(head(response_data[[2]], -1)), collapse = ""),
+    right            = paste(unlist(head(response_data[[3]], -1)), collapse = ""),
+    book             = response_data[[4]][[1]],
     stringsAsFactors = FALSE
   )
   
   if(metadata) {
-    X$chapter <- position_metadata[[1]]
-    X$paragraph <- position_metadata[[2]]
-    X$sentence <- position_metadata[[3]]
-    X$begin <- result_metadata[[2]]
-    X$end <- result_metadata[[3]]
+      result$chapter <- response_data[[5]][[1]]
+    result$paragraph <- response_data[[5]][[2]]
+     result$sentence <- response_data[[5]][[3]]
+        result$begin <- response_data[[4]][[2]]
+          result$end <- response_data[[4]][[3]]
   }
   
-  return(X)
+  return(result)
 }
 
 subset_query_list <- function(params) {
